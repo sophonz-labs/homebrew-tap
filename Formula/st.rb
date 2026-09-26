d122f76c5fca5a3788d4d3cbe832484a515dc07b
sophonz-release <noreply@sophonz.io>
2026-09-26 14:03:25 +0000

    st v0.3.15

--- Formula/st.rb
+++ Formula/st.rb
@@ -1,24 +1,24 @@
 class St < Formula
   desc "Share the terminal session you are already running"
   homepage "https://sophonz.io"
-  version "0.3.14"
+  version "0.3.15"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://dl.sophonz.io/v0.3.14/st-darwin-arm64"
-      sha256 "31af61c7bba497c15238a2056145c005438718a203c1ac42791f051b807c925b"
+      url "https://dl.sophonz.io/v0.3.15/st-darwin-arm64"
+      sha256 "e42ea751d72253156eedd52cc9bfdb898d2bd77ccfb79c13f17b74a87ec185da"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://dl.sophonz.io/v0.3.14/st-linux-x64"
-      sha256 "17354cc4359f58cedf0800c115d5a81f90aa375201de41d94297f3e4b4e08e3f"
+      url "https://dl.sophonz.io/v0.3.15/st-linux-x64"
+      sha256 "702e32fe88cdd1ecdd2d5abe1b29e2477b6792bd404e34de136a3360b9d442b2"
     end
     on_arm do
-      url "https://dl.sophonz.io/v0.3.14/st-linux-arm64"
-      sha256 "02bd96be59a9562c2be22ce2f27be87831bee8c9300af0aeb1dc6aae2efad924"
+      url "https://dl.sophonz.io/v0.3.15/st-linux-arm64"
+      sha256 "4242a23fb4940633299880efedf5ef5f30846533a1195dd3f220a0ff5d82fba1"
     end
   end
 
